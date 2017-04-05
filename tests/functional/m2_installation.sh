134d4e851b711208eb0438857ddf85d4bc1d9d1f
Yevgenii Yevtushenko <yyevtushenko@magento.com>
2017-04-05 18:19:24 +0300

    Add AssertEqual

--- tests/functional/m2_installation.sh
+++ tests/functional/m2_installation.sh
@@ -5,4 +5,20 @@ mkdir magento;
 cd magento;
 ../m2install.sh --force --source composer --ee -v 2.1.5
 
-[[ "$(php bin/magento -V --no-ansi)" != "Magento CLI version 2.1.5" ]] && return 1
+
+function assertEqual()
+{
+  if [[ "$1" == "$2" ]]
+  then
+    return 0;
+  else
+    echo $1;
+    echo $2;
+  fi
+  return 1;
+}
+
+INSTALLED_VERSION="$(php bin/magento -V --no-ansi)"
+echo "Installed Magento version: $INSTALLED_VERSION"
+
+assertEqual "$INSTALLED_VERSION" "Magento CLI version 2.1.5"
