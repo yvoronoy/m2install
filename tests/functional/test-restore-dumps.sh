1284140fdfb977c742bdc4a918d657b2f3efc102
Yaroslav Voronoy <yvoronoy@magento.com>
2017-04-15 16:27:36 +0300

    Fixed #107: When a deploy of dumps is complete check list of required folders and files

--- tests/functional/test-restore-dumps.sh
+++ tests/functional/test-restore-dumps.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 source tests/functional.sh
-${BIN_M2INSTALL} --force --source composer --ee -v 2.1.5
+${BIN_M2INSTALL} --force --source composer --ee -v 2.1.5 --quiet
 bin/magento setup:backup --code --db
 ls -A | grep -v var | xargs rm -rf
 cp var/backups/* ./
 rm -rf var
 
-${BIN_M2INSTALL} -f
+${BIN_M2INSTALL} -f --quiet
 
 assertEqual $(ls app/etc/env.php.merchant) app/etc/env.php.merchant "Original file env.php.merchant has been created"
 
