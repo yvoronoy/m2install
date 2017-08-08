c8ede18d2be90ef84e0e7d994154ef2202c6895a
Andrey Abumuslimov <aabumuslimov@magento.com>
2017-08-08 16:43:59 -0700

    Fix for a slash count for tar command caused by OSX wc output formatting

--- m2install.sh
+++ m2install.sh
@@ -217,7 +217,7 @@ function getStripComponentsValue()
 {
     local stripComponents=
     local slashCount=
-    slashCount=$(tar -tf "$1" | grep -v vendor | fgrep pub/index.php | sed 's/pub[/]index[.]php//' | sort | head -1 | tr -cd '/' | wc -c)
+    slashCount=$(tar -tf "$1" | grep -v vendor | fgrep pub/index.php | sed 's/pub[/]index[.]php//' | sort | head -1 | tr -cd '/' | wc -m | tr ' ')
 
     if [[ "$slashCount" -gt 0 ]]
     then
