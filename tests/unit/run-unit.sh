bd4c39fdf145dd612a9eab8ca467cd28e3486a00
Yaroslav Voronoy <yvoronoy@magento.com>
2017-08-19 04:12:30 +0000

    Add unit to travis

--- tests/unit/run-unit.sh
+++ tests/unit/run-unit.sh
@@ -7,6 +7,7 @@ function loadAssertFramework()
     if [ ! -f assert.sh ]
     then
         wget https://raw.github.com/lehmannro/assert.sh/v1.1/assert.sh
+        mv assert.sh $(getUnitTestDirectory)/assert.sh
     fi
 
     source assert.sh
@@ -14,19 +15,20 @@ function loadAssertFramework()
 
 function getUnitTestDirectory()
 {
-    echo $(getProjectRootDirectory)tests/unit/;
+    echo $(getProjectRootDirectory)/tests/unit;
 }
 
 function getProjectRootDirectory()
 {
-    echo "$(pwd)/../../"
+    echo "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../..";
+    return 0;
 }
 
 ####################################
 
 loadAssertFramework;
-source $(getProjectRootDirectory)src/functions.sh
-for file in $(getUnitTestDirectory)tests/test*.sh
+source $(getProjectRootDirectory)/src/functions.sh
+for file in $(getUnitTestDirectory)/tests/test*.sh
 do
     source ${file}
 done
--- tests/unit/tests/testGetDbName.sh
+++ tests/unit/tests/testGetDbName.sh
@@ -3,21 +3,17 @@
 CURRENT_DIR_NAME=$(basename $(pwd));
 setRequest dbUser 'root'
 setRequest basePath 'a/b'
-
 assert getDbName 'root_a_b';
 
 setRequest dbUser ''
 setRequest basePath ''
-
-assert getDbName 'unit';
+assert getDbName ${CURRENT_DIR_NAME};
 
 setRequest dbUser 'root'
 setRequest basePath ''
-
-assert getDbName 'root_unit';
+assert getDbName "root_${CURRENT_DIR_NAME}";
 
 setRequest dbUser ''
 setRequest basePath 'some-path/abc/'
-
 assert getDbName 'somepath_abc';
 
