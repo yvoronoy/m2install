98bf0c5ca4b2a605d6c57a174b65b088e81c06e1
Yarik Voronyi <yvoronoy@adobe.com>
2021-12-27 00:12:35 -0800

    update github actions

--- tests/testsuite-parallel.sh
+++ tests/testsuite-parallel.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Run new testsuite"
-JOBS="${1:-3}"
-parallel --eta --shuf -j$JOBS --halt now,fail=1 bash tests/functional/{} ::: $(ls tests/functional)
+parallel --shuf --halt now,fail=1 bash tests/functional/{} ::: $(ls tests/functional)
