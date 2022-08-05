90a52c4ef4908abf7839cdcf96216311f6442009
Malith Wimalagunarathne <diyoguha@adobe.com>
2022-08-05 16:35:21 +1000

    --live-search flag added

--- m2install.sh
+++ m2install.sh
@@ -41,6 +41,7 @@ EE_PATH=magento2ee
 INSTALL_EE=
 INSTALL_B2B=
 INSTALL_PR=
+INSTALL_LS=
 CONFIG_NAME=.m2install.conf
 USE_WIZARD=1
 
@@ -484,8 +485,8 @@ function foundSupportBackupFiles()
 function validateDatabaseDumpArchive()
 {
   local minSizeLimit=2
-  local dbDumpFilenamePath="$(getDbDumpFilename)" 
-  local codeDumpFilenamePath="$(getCodeDumpFilename)" 
+  local dbDumpFilenamePath="$(getDbDumpFilename)"
+  local codeDumpFilenamePath="$(getCodeDumpFilename)"
   local dbDumpFileSize="$(wc -c ${dbDumpFilenamePath} | awk '{print $1}')"
   local codeDumpFileSize="$(wc -c ${codeDumpFilenamePath} | awk '{print $1}')"
   [ "$dbDumpFileSize" -lt "$minSizeLimit" ] && { printErrorAndExit 255 "MySQL DB Dump is corrupt. For on-prem, please request a new MySQL Dump from the merchant and ensure it is created using the mysqldump utility and not bin/magento support:db:backup. For Magento-Cloud, please regenerate a new MySQL Dump by using the ZD Dump Widget / cloud-teleport."; }
@@ -545,6 +546,10 @@ function noSourceWizard()
     then
          INSTALL_PR=1
     fi
+    if askConfirmation "Do you want install Magento Live Search (y/N)"
+    then
+         INSTALL_LS=1
+    fi
 }
 
 function printConfirmation()
@@ -601,6 +606,10 @@ function printConfirmation()
     if [ "${INSTALL_PR}" ]
     then
         printString "Magento Product Recommendations will be installed."
+    fi
+    if [ "${INSTALL_LS}" ]
+    then
+        printString "Magento Live Search will be installed."
     fi
 }
 
@@ -686,6 +695,7 @@ MAGENTO_VERSION=$MAGENTO_VERSION
 INSTALL_EE=$INSTALL_EE
 INSTALL_B2B=$INSTALL_B2B
 INSTALL_PR=$INSTALL_PR
+INSTALL_LS=$INSTALL_LS
 GIT_CE_REPO=$GIT_CE_REPO
 GIT_EE_REPO=$GIT_EE_REPO
 MAGE_MODE=$MAGE_MODE
@@ -1105,7 +1115,7 @@ function configure_db()
   printString "Updating Database Configuration"
   setConfig 'web/secure/base_url' "${BASE_URL}";
   setConfig 'web/unsecure/base_url' "${BASE_URL}";
-  setConfig 'web/secure/offloader_header' 'X-Forwarded-Proto';  
+  setConfig 'web/secure/offloader_header' 'X-Forwarded-Proto';
   setConfig 'google/analytics/active' '0';
   setConfig 'google/adwords/active' '0';
   setConfig 'msp_securitysuite_twofactorauth/general/enabled' '0';
@@ -1650,7 +1660,7 @@ function installLiveSearch()
     runCommand
   fi
 
-  $BIN_PHP $BIN_MAGE module:status | grep -E 'Magento_Elasticsearch*|Magento_AdvancedSearch|Magento_InventoryElasticsearch' | grep -v List | grep -v None | grep -v -e '^$' | xargs $BIN_PHP $BIN_MAGE module:disable
+  $BIN_PHP $BIN_MAGE module:status | grep -E 'Magento_Elasticsearch*|Magento_InventoryElasticsearch' | grep -v List | grep -v None | grep -v -e '^$' | xargs $BIN_PHP $BIN_MAGE module:disable
   $BIN_PHP $BIN_MAGE module:status | grep Magento_LiveSearch | grep -v List | grep -v None | grep -v -e '^$' | xargs $BIN_PHP $BIN_MAGE module:enable
   $BIN_PHP $BIN_MAGE --quiet config:set  'catalog/search/engine' NULL
   $BIN_PHP $BIN_MAGE module:status | grep -E '.*ServicesId*|.*ServicesConnector*|.*DataExporter*|.*SaaS*|.*DataServices*'| xargs $BIN_PHP $BIN_MAGE  module:enable
@@ -1769,7 +1779,7 @@ function installMagento()
         # Install Bundled Extensions for version 2.2.+
         if [[ $MAGENTO_VERSION =~ ^2\.[^01]\..* ]]
         then
-            for be in "${BUNDLED_EXTENSION[@]}" 
+            for be in "${BUNDLED_EXTENSION[@]}"
             do
                 CMD="composer require --quiet ${be}"
                 runCommand
@@ -1985,6 +1995,10 @@ showComposerWizzard()
     then
         INSTALL_PR=1
     fi
+    if askConfirmation "Do you want install Magento Live Search (y/N)"
+    then
+        INSTALL_LS=1
+    fi
 }
 
 printComposerConfirmation()
@@ -2128,7 +2142,7 @@ function isInputNegative()
 function validateStep()
 {
     local _step=$1;
-    local _steps="restore_db restore_code configure_db configure_files configure installB2B installPrex add_remote"
+    local _steps="restore_db restore_code configure_db configure_files configure installB2B installPrex installLiveSearch add_remote"
     if echo "$_steps" | grep -q "$_step"
     then
         if type -t "$_step" &>/dev/null
@@ -2296,6 +2310,7 @@ Options:
     --ee                                 Install Enterprise Edition.
     --b2b                                Install B2B Extension.
     --prex                               Install Product Recommendations.
+    --live-search                        Install Live Search
     -v, --version                        Magento Version - it means: Composer version or GIT Branch
     --mode (dev, prod)                   Magento Mode. Dev mode does not generate static & di content.
     --quiet                              Quiet mode. Suppress output all commands
@@ -2367,6 +2382,9 @@ function processOptions()
             ;;
             --prex)
                 INSTALL_PR=1
+            ;;
+            --live-search)
+                INSTALL_LS=1
             ;;
             -b|--git-branch)
                 # @DEPRECATED. Use -v or --version instead
@@ -2524,6 +2542,10 @@ function magentoInstallAction()
     if [[ "$INSTALL_PR" ]]
     then
         addStep "installPrex"
+    fi
+    if [[ "$INSTALL_LS" ]]
+    then
+        addStep "installLiveSearch"
     fi
 }
 
